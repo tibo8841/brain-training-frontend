c9c9d12b3086a6a62bddb51941cf2f73551d5c8d
tibo8841 <thibaut.hucker@gmail.com>
2022-05-30 14:47:33 +0100

    initial react app

---
+++ brain-training/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Brain training</h1>
+      <img
+        style={{ height: "70vh" }}
+        alt="Dr. Alex brain train"
+        src="/Screenshot 2022-05-30 at 11.49.21.png"
+      ></img>
+    </div>
+  );
+}
+
+export default App;
