df73cbfe72240642c13bd396066ef245a732e838
Milan Patel <milanpat42@gmail.com>
2022-06-07 11:29:19 +0100

    new questions\

--- brain-training/src/GameQuestions/MathsQuestions.js
+++ brain-training/src/GameQuestions/MathsQuestions.js
@@ -45,5 +45,114 @@ export function MathsQuestions() {
         { answer: 50, correct: false },
       ],
     },
+
+    {
+      Question: "What is 150 / 50?",
+      Answers: [
+        { answer: 15, correct: false },
+        { answer: 30, correct: false },
+        { answer: 3, correct: true },
+        { answer: 5, correct: false },
+      ],
+    },
+    {
+      Question: "What is 15 * 5?",
+      Answers: [
+        { answer: 75, correct: true },
+        { answer: 65, correct: false },
+        { answer: 80, correct: false },
+        { answer: 55, correct: false },
+      ],
+    },
+    {
+      Question: "What is 13-8?",
+      Answers: [
+        { answer: 7, correct: false },
+        { answer: 4, correct: false },
+        { answer: 5, correct: true },
+        { answer: 6, correct: false },
+      ],
+    },
+    {
+      Question: "What is 40 + 66?",
+      Answers: [
+        { answer: 116, correct: false },
+        { answer: 106, correct: true },
+        { answer: 96, correct: false },
+        { answer: 100, correct: false },
+      ],
+    },
+    {
+      Question: "What is 21 + 45 + 6?",
+      Answers: [
+        { answer: 62, correct: false },
+        { answer: 72, correct: true },
+        { answer: 69, correct: false },
+        { answer: 79, correct: false },
+      ],
+    },
+    {
+      Question: "What is 49 + 61 - 25?",
+      Answers: [
+        { answer: 95, correct: false },
+        { answer: 85, correct: true },
+        { answer: 90, correct: false },
+        { answer: 75, correct: false },
+      ],
+    },
+    {
+      Question: "What is 3 * 18?",
+      Answers: [
+        { answer: 54, correct: true },
+        { answer: 59, correct: false },
+        { answer: 45, correct: false },
+        { answer: 49, correct: false },
+      ],
+    },
+    {
+      Question: "What is 4 * 8?",
+      Answers: [
+        { answer: 28, correct: false },
+        { answer: 36, correct: false },
+        { answer: 34, correct: false },
+        { answer: 32, correct: true },
+      ],
+    },
+    {
+      Question: "What is 64 * 3?",
+      Answers: [
+        { answer: 129, correct: false },
+        { answer: 219, correct: false },
+        { answer: 921, correct: false },
+        { answer: 192, correct: true },
+      ],
+    },
+    {
+      Question: "What is 7200 / 900?",
+      Answers: [
+        { answer: 7, correct: false },
+        { answer: 6, correct: false },
+        { answer: 8, correct: true },
+        { answer: 9, correct: false },
+      ],
+    },
+    {
+      Question: "What is 95 - 49 +32?",
+      Answers: [
+        { answer: 78, correct: true },
+        { answer: 89, correct: false },
+        { answer: 56, correct: false },
+        { answer: 67, correct: false },
+      ],
+    },
+    {
+      Question: "What is 800 / 32?",
+      Answers: [
+        { answer: 35, correct: false },
+        { answer: 25, correct: true },
+        { answer: 30, correct: false },
+        { answer: 64, correct: false },
+      ],
+    },
   ];
 }
